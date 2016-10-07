915eb7b6c37b63dc7daa9665a031c0955c822db8
tao <land20150303@126.com>
2016-10-07 12:57:19 +0800

    init commit.

---
+++ app/src/main/java/com/china/use_cv/MainActivity.java
@@ -0,0 +1,14 @@
+package com.china.use_cv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+        }
+}
